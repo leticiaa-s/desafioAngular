8b6a3b830613d3bcaa224cf1ba5deb968a1af78f
leticiaa-s <leticiasqr97@gmail.com>
2024-04-09 19:42:22 -0300

    feat:components header, footer and products

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ProductsComponent } from './components/products/products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule
